a16a012cf76ec1048879990a22658bb8b2d6ad5f
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-12-09 07:44:50 -0330

    Updates

---
+++ precache-manifest.fe5c6fc9203f51ed8c1aa2c812cc7394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3dc1444e4242c6d17ef559e1e284565",
+    "url": "/rmp-prototype-frontend/index.html"
+  },
+  {
+    "revision": "6bb3b9097d091957a7d5",
+    "url": "/rmp-prototype-frontend/static/css/main.dd0f6a64.chunk.css"
+  },
+  {
+    "revision": "26b8ec819558448e0875",
+    "url": "/rmp-prototype-frontend/static/js/2.395a7b7b.chunk.js"
+  },
+  {
+    "revision": "6bb3b9097d091957a7d5",
+    "url": "/rmp-prototype-frontend/static/js/main.e4608c4b.chunk.js"
+  },
+  {
+    "revision": "5df843ce48c42dec84ea",
+    "url": "/rmp-prototype-frontend/static/js/runtime-main.fae3ae94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cmp-prototype-frontend/precache-manifest.9771ce2591a6a1ea5ee7ca2149a891de.js"
+  "/rmp-prototype-frontend/precache-manifest.fe5c6fc9203f51ed8c1aa2c812cc7394.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cmp-prototype-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rmp-prototype-frontend/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
